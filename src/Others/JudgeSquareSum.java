5fac80b2852836e5d077c6abf413a38b680d8f17
RapGod <1832591415@my.swjtu.edu.cn>
2021-05-01 15:19:25 +0800

    a

--- src/Others/JudgeSquareSum.java
+++ src/Others/JudgeSquareSum.java
@@ -1,4 +1,20 @@
 package Others;
 
 public class JudgeSquareSum {
+
+    public static boolean judgeSquareSum(int c) {
+        if (c < 0)
+            return false;
+        int mid = (int) Math.sqrt(c);
+        for (int i = 0; i <= mid; i++) {
+            int temp = c - (int) Math.pow(i, 2);
+            if (Math.pow((int) Math.sqrt(temp), 2) == temp)
+                return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(judgeSquareSum(2));
+    }
 }
